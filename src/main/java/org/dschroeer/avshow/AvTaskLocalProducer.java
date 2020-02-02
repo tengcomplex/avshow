fccc6045c7a69f35ccbde3b65d076d6a964a3960
David Schröer <tengcomplex@gmail.com>
2020-02-02 13:47:37 +0100

    Improve error handling

--- src/main/java/org/dschroeer/avshow/AvTaskLocalProducer.java
+++ src/main/java/org/dschroeer/avshow/AvTaskLocalProducer.java
@@ -2,6 +2,7 @@ package org.dschroeer.avshow;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,16 +20,15 @@ public class AvTaskLocalProducer implements AvTaskProducer {
     try {
       String task = RuntimeCommand.getFirstLineFromProcess(Config.LOCAL_PRODUCER_COMMAND);
 System.out.println("AvTaskLocalProducer.next(), task:" + task);
+      if (task == null) {
+        throw new RuntimeException("No result reading from " + Arrays.toString(Config.LOCAL_PRODUCER_COMMAND));
+      }
       ObjectMapper objectMapper = new ObjectMapper();
       AvTask avTask = objectMapper.readValue(task, AvTask.class);
 System.out.println("AvTaskLocalProducer.next(), myAvTask:" + avTask);
       return avTask;
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (InterruptedException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+    } catch (IOException | InterruptedException | RuntimeException e) {
+      L.log(Level.WARNING, "Error ", e);
     }
     return new AvTask("", "Unknown");
   }
